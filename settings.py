3c60e913ec79b5943b4ce519525bd86f0ca677ae
lucaslima18 <lucas.ala1999@gmail.com>
2020-04-14 11:24:19 -0300

    ajustings done

--- settings.py
+++ settings.py
@@ -1,26 +1,16 @@
-import argparse
+import argparse, os
+from dotenv import load_dotenv
 
-parser = argparse.ArgumentParser(description="")
-parser.add_argument("--repository_uuid", required=True, type=str)
-parser.add_argument("--auth_token", required=True, type=str)
+parser = argparse.ArgumentParser()
 parser.add_argument("--language", required=False,default="pt-br", type=str)
-parser.add_argument("--service", required=True, type=str)
+parser.add_argument("--service", required=False, default="null", type=str)
 arguments = parser.parse_args()
 
-REPOSITORY_UUID = arguments.repository_uuid
 LANGUAGE = arguments.language
-AUTH_TOKEN = arguments.auth_token
+SERVICE_TYPE = arguments.service
+REPOSITORY_UUID = "c4bf350f-e678-490e-b5ca-c0a2b1b52b93"
+AUTH_TOKEN = load_dotenv(AUTH_TOKEN)
 
-if (
-    arguments.service == "clear"
-    or arguments.service == "testing"
-    or arguments.service == "training"
-):
-    SERVICE_TYPE = arguments.service
+print(AUTH_TOKEN)
 
-else:
-    #deixar textos mais completos aqui
-    print("\nThis option is not avaliable, please reload and try one of this options:\n")
-    print("clear\ntesting\ntraining\n")
-    exit()
 
